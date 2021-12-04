7fc937988b3a27346d12c3883c866be50180d6a3
emilianoduquep <emiliano.duque.p@gmail.com>
2021-12-04 11:10:16 -0500

    se hizo un ejemplo de crear un componente y una vista

---
+++ Sprint4/vue_ecotur/src/components/Navegacion.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navegacion">
+        <li>Inicio de Sesion</li>
+        <li>Acerca de Nosotros</li>
+    </nav>
+</template>
+
+<style scoped>
+.navegacion{
+    display: flex;
+    justify-content:flex-end;
+    list-style-type: none;
+}
+li{
+    padding: 8px;
+    margin-right: 2px;
+    background-color: rgb(223, 133, 133);
+}
+</style>
+
+<script>
+export default {
+    name:"Navegacion",
+    data: function (){
+        return {
+            varibale1:1,
+        }
+    }
+}
+</script>
+
+
---
+++ Sprint4/vue_ecotur/src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    
+    <Navegacion/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Navegacion from "@/components/Navegacion"
+
+
+export default {
+  name: "Login",
+  components: {
+   Navegacion,
+  },
+  methods:{
+
+  },
+  data: function(){
+      return {
+
+      }
+  }
+};
+</script>

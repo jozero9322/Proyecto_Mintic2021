96c307456989af8b5e6d320e9a4fa7789cda80f2
jozero9322 <51867175+jozero9322@users.noreply.github.com>
2021-12-04 12:56:41 -0500

    Update Login.vue
    Se realiza las modificaciones del Login para incluir los espacios para ingresar los datos de usuario

--- Sprint4/vue_ecotur/src/views/Login.vue
+++ Sprint4/vue_ecotur/src/views/Login.vue
@@ -1,29 +1,60 @@
 <template>
-  <div >
-    
-    <Navegacion/>
+  <div class="Login-form">
+    <Navegacion />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h1>Inicio de Sesion</h1>
+    <form action class="form">
+      <label class="form-label" for="#Usuario">Usuario:</label>
+      <input
+        class="form-input"
+        type="Usuario"
+        id="Usuario"
+        required
+        placeholder="Usuario"
+      />
+      <label class="form-label" for="#Contraseña">Contraseña:</label>
+      <input
+        class="form-input"
+        type="Contraseña"
+        id="Contraseña"
+        placeholder="Contraseña"
+      />
+      <input class="form-submit" type="submit" value="Login" />
+    </form>
   </div>
 </template>
+
+<style scoped>
+.form {
+  width: 30%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  border-radius: 5px;
+  line-height: 20pt;
+}
+.form {
+  background-color: rgba(0, 128, 0, 0.404);
+}
+</style>
 
 <script>
 // @ is an alias to /src
 
-import Navegacion from "@/components/Navegacion"
-
+import Navegacion from "@/components/Navegacion";
 
 export default {
   name: "Login",
   components: {
-   Navegacion,
+    Navegacion,
   },
-  methods:{
-
+  methods: {},
+  data: function () {
+    return {};
   },
-  data: function(){
-      return {
-
-      }
-  }
 };
 </script>
+
+<style></style>

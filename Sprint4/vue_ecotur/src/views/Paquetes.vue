821b575bcb82363f5b35266d6f04f81e6ef3f2b5
emilianoduquep <emiliano.duque.p@gmail.com>
2021-12-04 12:33:43 -0500

    se creo la vista de Paquetes.vue y un componente Paque.vue

---
+++ Sprint4/vue_ecotur/src/views/Paquetes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div >
+    
+    <Navegacion/>
+    <Paque/>
+     
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Navegacion from "@/components/Navegacion"
+import Paque from "@/components/Paque"
+
+
+export default {
+  name: "Paquetes",
+  components: {
+   Navegacion,
+   Paque,
+  },
+};
+</script>

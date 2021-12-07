73a7273c50290c4b7e4a1475d84d3a03de1a8ff5
johnfredy09 <john.gonzalez2@o365.unab.edu.co>
2021-12-06 20:14:08 -0500

    vistaBoyaca

--- Sprint4/vue_ecotur/src/router/index.js
+++ Sprint4/vue_ecotur/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Paquetes from "../views/Paquetes.vue";
+import Boyaca from "../views/Boyaca.vue";
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/Boyaca",
+    name: "Boyaca",
+    component: Boyaca,
   },
   {
     path: "/login",

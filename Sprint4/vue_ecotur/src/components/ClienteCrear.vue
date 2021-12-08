1141d6634d74ac51d9d1616073e6726ea30920a2
emilianoduquep <emiliano.duque.p@gmail.com>
2021-12-08 17:08:55 -0500

    trabajando el el CRUD de clientes

---
+++ Sprint4/vue_ecotur/src/components/ClienteCrear.vue
@@ -0,0 +1,144 @@
+<template>
+<div>
+
+    <div class="contenedor">
+        <div>
+            <h3>Crear Clientes</h3>
+            <form class="formulario__registro" action="#">
+
+                <label class="formulario__registro-inputs" for="cedula">Cédula</label>
+                <input class="formulario__registro-inputs" required type="text" placeholder="Cédula" id="cedula">
+
+                <label class="formulario__registro-inputs" for="nombre">Nombre</label>
+                <input class="formulario__registro-inputs" required type="text" placeholder="Nombre del Cliente" id="nombre">
+
+                <label class="formulario__registro-inputs" for="usuario">Usuario</label>
+                <input class="formulario__registro-inputs" required type="text" placeholder="Usuario" id="usuario">
+
+                <label class="formulario__registro-inputs" for="password">Password</label>
+                <input class="formulario__registro-inputs" required type="text" placeholder="Password" id="password">
+
+
+            </form>
+            <div class="formulario-botones">  
+                <button type="button" @click="limpiar">Limpiar</button>
+                <button type="button" @click="agregarCliente">Crear Cliente</button>
+
+            </div>
+        </div>
+        <div class="contenedor__listado">
+            <h3>Listado de Clientes</h3>
+            
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Cédula</th>
+                    <th>Nombre</th>
+                    <th>Usuario</th>
+                    <th>Password</th>
+
+                </tr>
+            </thead>
+
+            <tbody >
+                <tr v-for="eachPackage in allClients" :key="eachPackage">
+                    <td>{{eachPackage.cedula}}</td>
+                    <td>{{eachPackage.nombre}}</td>
+                    <td>{{eachPackage.usuario}}</td>
+                    <td>{{eachPackage.password}}</td>
+
+                </tr>   
+                            
+            </tbody>
+
+        </table>
+
+        </div>
+    </div>
+</div>
+</template>
+
+
+<script>
+import api from "@/logic/api.js";
+
+export default {
+    name:"Paquetes",
+    data: function(){
+        return {
+            allClients: [],
+
+        }
+
+
+    },
+    methods: {
+
+        async agregarCliente(){
+            const _cedula = document.getElementById('cedula').value;
+            const _nombre = document.getElementById('nombre').value;
+            const _usuario = document.getElementById('usuario').value;
+            const _password = document.getElementById('password').value;
+
+
+            if ( _cedula != "" && _nombre != "" && _usuario != "" && _password != "" ) {
+                await api.create("cliente",{cedula:_cedula, nombre:_nombre, usuario: _usuario, password: _password});
+                this.actualiza();
+            } else {
+                alert('Ingrese todos los datos');
+            }         
+        },
+        limpiar(){
+            document.getElementById('cedula').value = "";
+            document.getElementById('nombre').value = "";
+            document.getElementById('usuario').value = "";
+            document.getElementById('password').value = "";
+        },
+        async actualiza(){
+            const resp = await api.getAll("clientes");
+            this.allClients = resp.data;
+    }
+
+    },
+    async mounted(){
+
+        const respPaq = await api.getAll("clientes");
+        this.allClients = respPaq.data;
+
+    },
+
+}
+
+</script>
+
+
+
+<style scoped>
+.contenedor{
+    display: grid;
+    grid-template-columns: 1fr 1.5fr;
+}
+.formulario__registro{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background-color: rgb(213, 211, 208);
+    padding: 10px;
+    border-radius: 5px;
+    margin-left: 40px;
+
+}
+.formulario__registro-inputs{
+    padding: 2px;
+    margin: 5px;
+}
+.formulario-botones{
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    margin-left: 40px;
+}
+.contenedor__listado {
+    align-items: center;
+    margin: 0 auto;
+}
+</style>
---
+++ Sprint4/vue_ecotur/src/components/ClienteEditar.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="contenedor">
+        <div>
+            <h3>Editar Clientes</h3>
+
+            <form class="formulario__registro" action="#">
+
+                <label v-show="siEditar" class="formulario__registro-inputs" for="cedula">Cédula</label>
+                <input v-show="siEditar" class="formulario__registro-inputs" required type="text" placeholder="Cédula" id="cedula">
+
+                <label v-show="siEditar" class="formulario__registro-inputs" for="nombre">Nombre</label>
+                <input v-show="siEditar" class="formulario__registro-inputs" required type="text" placeholder="Nombre del Cliente" id="nombre">
+
+                <label class="formulario__registro-inputs" for="usuario">Usuario</label>
+                <input class="formulario__registro-inputs" required type="text" placeholder="Usuario" id="usuario">
+
+                <label class="formulario__registro-inputs" for="password">Password</label>
+                <input class="formulario__registro-inputs" required type="text" placeholder="Password" id="password">
+
+
+            </form>
+            <div class="formulario-botones">  
+                <button type="button" @click="limpiar">Limpiar</button>
+                <button v-if="!codigo" type="button" @click="consultarDatos">Datos</button>
+                <button v-if="codigo" type="button" @click="editarCliente">Editar Cliente</button>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import api from "@/logic/api.js";
+
+export default {
+    name:"Clientes",
+    data: function(){
+        return {
+            allClients: [],
+            codigo:false,
+            siEditar:false,
+
+        }
+
+
+    },
+    methods: {
+        async consultarDatos(){
+            const consultarUsuario = document.getElementById('usuario').value;
+            const consultarPassword = document.getElementById('password').value;
+            console.log(consultarUsuario);
+            console.log(consultarPassword);
+            let respuesta = await api.getOneClient('cliente',{usuario:consultarUsuario,password:consultarPassword});
+            console.log(respuesta);
+            if ( respuesta.data.usuario == consultarUsuario) {
+                this.codigo = true;
+                this.siEditar = true;
+                document.getElementById('cedula').value = respuesta.data.cedula;
+                document.getElementById('nombre').value = respuesta.data.nombre;
+                document.getElementById('usuario').value = respuesta.usuario;
+                document.getElementById('password').value = respuesta.data.password;
+
+            } else {
+                console.log("No hay resultado");
+            }
+            
+        },
+        async editarCliente(){
+            const _cedula = document.getElementById('cedula').value;
+            const _nombre = document.getElementById('nombre').value;
+            const _usuario = document.getElementById('usuario').value;
+            const _password = document.getElementById('password').value;
+
+            if ( _cedula != "" && _nombre != "" && _usuario != "" && _password != "" ) {
+                await api.edit(`cliente/${_cedula}`,{cedula:_cedula, nombre:_nombre, usuario: _usuario, password:_password});
+                this.actualiza();
+                this.limpiar();
+            } else {
+                alert('Ingrese todos los datos');
+            }         
+        },
+        limpiar(){
+            document.getElementById('cedula').value = "";
+            document.getElementById('nombre').value = "";
+            document.getElementById('usuario').value = "";
+            document.getElementById('password').value = "";
+        },
+        async actualiza(){
+            const resp = await api.getAll("clientes");
+            this.allClients = resp.data;
+    }
+
+    },
+    async mounted(){
+
+        const resp = await api.getAll("clientes");
+        this.allClients = resp.data;
+    },
+
+}
+
+</script>
+
+
+
+<style scoped>
+.contenedor{
+    width: 40%;
+    margin: 0 auto;
+}
+.formulario__registro{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background-color: rgb(213, 211, 208);
+    padding: 10px;
+    border-radius: 5px;
+    margin-left: 20px;
+
+}
+.formulario__registro-inputs{
+    padding: 2px;
+    margin: 5px;
+}
+.formulario-botones{
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    margin-left: 20px;
+}
+</style>
